4b44fbfc7d3581a0b94e4ca2d236b6abd693c0e8
springade27 <springade27@gmail.com>
2019-12-23 14:19:43 +0900

    # 테이블 생성, 변경, 제약조건 추가

--- Scripts/MySQL_study.sql
+++ Scripts/MySQL_study.sql
@@ -10,3 +10,48 @@ grant all privileges on mysql_study.* to 'mysql_study'@'localhost' identified by
 grant all privileges on mysql_study.* to 'mysql_study'@'%' identified by 'rootroot';
 
 
+-- 부서 테이블 생성
+create table department (
+	deptno int(11) not null,
+	deptname char(10),
+	floor int(11) default 0,
+	primary key (deptno)
+);
+
+-- 사원 테이블 생성
+create table employee (
+	empno int(11) not null,
+	empname varchar(20) not null,
+	title varchar(10) default '사원',
+	manager int(11),
+	salary int(11),
+	dno int(11) default 1
+);
+
+-- 제약조건 추가
+alter table employee add constraint primary key(empno);
+alter table employee add constraint fk_employee_manager foreign key(manager) references employee(empno);
+alter table employee add constraint fk_employee_dno foreign key(dno) references department(deptno)
+on delete no action 
+on update cascade;
+
+-- 제약조건 확인
+select * from information_schema.table_constraints;
+desc information_schema.table_constraints;
+
+-- 테이블 정보 보기
+desc employee;
+
+-- 데이블 속성 변경
+alter table employee rename to emp;   -- 테이블 이름 변경 --
+alter table emp rename to employee;
+
+alter table employee add column phone char(13);  -- 컬럼 추가 --
+alter table employee drop column phone;          -- 컬럼 삭제 --
+alter table employee add column phone char(13) first;
+
+alter table employee change column empname name char(30);		
+alter table employee modify column title char(10);		
+alter table employee change column name empname varchar(20);	-- 컬럼 명, 타입 변경 --
+
+
